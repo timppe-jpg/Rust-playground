d0cead96de03cfb2342d7f12f26669ff81d3f59b
timppe12 <timppe.jarvinen@hotmail.com>
2022-11-30 11:58:04 +0200

    asd

--- Rust-playground/src/main.rs
+++ Rust-playground/src/main.rs
@@ -1,8 +1,17 @@
+use classTest::Post;
+
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("Hello");
+    string_test(&s);
     test(5, 10);
+
+    let test = classTest::Car{};
 }
 
 fn test(number1: i32, number2: i32){
     println!("{}", number1 + number2);
+}
+
+fn string_test(text: &str){
+    println!("{}", text);
 }
